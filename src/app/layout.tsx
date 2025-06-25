4ce2538c5b9527f325db11fa3f17dffcc84aa102
kitabmantra <kitabmantra08@gmail.com>
2025-06-25 08:12:12 +0000

    vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import QueryProvider from "@/lib/providers/QueryProvider";
 import { Toaster } from "react-hot-toast";
+import { Analytics } from "@vercel/analytics/next"
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -57,6 +58,7 @@ export default function RootLayout({
           {children}
         </QueryProvider>
         <Toaster />
+        <Analytics />
       </body>
     </html>
   );
